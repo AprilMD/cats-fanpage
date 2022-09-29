74cc3e3e2b4b7f3d2643dce974425b12e41a3171
April Dawson <aprilmdawson@hotmail.com>
2022-09-29 11:41:27 +0100

    add Create and Read functions of blog

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    validates :title, presence: true
+    # first validation declares that a title value must be present - bc it's a string, the title value must contain at least one non-whitespace character.
+    validates :body, presence: true, length: { minimum: 10 }
+    # second validation declares that a body value must also be present and value must be at least 10 characters long.
 end
